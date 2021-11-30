01dbe93b5586c5658cb51f73837a153d1a87749c
khaerunisaikna <90880828+khaerunisaikna@users.noreply.github.com>
2021-11-30 13:19:47 +0700

    Add files via upload

--- ModelBuku.php
+++ ModelBuku.php
@@ -34,34 +34,34 @@ class ModelBuku extends CI_Model
         return $this->db->get()->row($field);
     }
     
-    //manajemen kategori
-    public function getKategori()
-    {
-        return $this->db->get('kategori');
-    }
-    public function kategoriWhere($where)
-    {
-        return $this->db->get_where('kategori', $where);
-    }
-    public function simpanKategori($data = null)
-    {
-        $this->db->insert('kategori', $data);
-    }
-    public function hapusKategori($where = null)
-    {
-        $this->db->delete('kategori', $where);
-    }
-    public function updateKategori($where = null, $data = null)
-    {
-        $this->db->update('kategori', $data, $where);
-    }
-    //join
-    public function joinKategoriBuku($where)
-    {
-        $this->db->select('buku.id_kategori,kategori.kategori');
-        $this->db->from('buku');
-        $this->db->join('kategori','kategori.id = buku.id_kategori');
-        $this->db->where($where);
-        return $this->db->get();
-    }
+//manajemen kategori
+public function getKategori()
+{
+    return $this->db->get('kategori');
+}
+public function kategoriWhere($where)
+{
+    return $this->db->get_where('kategori', $where);
+}
+public function simpanKategori($data = null)
+{
+    $this->db->insert('kategori', $data);
+}
+public function hapusKategori($where = null)
+{
+    $this->db->delete('kategori', $where);
+}
+public function updateKategori($where = null, $data = null)
+{
+    $this->db->update('kategori', $data, $where);
+}
+//join
+public function joinKategoriBuku($where)
+{
+    $this->db->select('buku.id_kategori,kategori.kategori');
+    $this->db->from('buku');
+    $this->db->join('kategori','kategori.id = buku.id_kategori');
+    $this->db->where($where);
+    return $this->db->get();
+}
 }
